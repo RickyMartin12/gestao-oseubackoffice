c0e9613edb7d3188502b43b788b94b9b6735caf2
RickyMartin12 <ricardopeleira16@gmail.com>
2023-03-16 09:12:15 +0000

    first commit2

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
-
+//mysql://b77485626682e7:44f82f1b@us-cdbr-east-06.cleardb.net/heroku_6523dff62d5f345?reconnect=true
 date_default_timezone_set('Europe/Lisbon');//or change to whatever timezone you want
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "oseuback_admin_gestao";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b77485626682e7";
+$password = "44f82f1b";
+$dbname = "heroku_6523dff62d5f345";
 
 
 
